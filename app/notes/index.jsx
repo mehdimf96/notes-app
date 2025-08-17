7fc154d5d4101a5e7f9ad4624d1302a95cec15b7
mehdimf96 <mehranfar.7575@gmail.com>
2025-08-17 15:05:22 +0330

    lazy load

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -10,8 +10,11 @@ import AddNoteModal from "../../components/AddNoteModal";
 import NoteList from "../../components/NoteList";
 
 const NoteScreen = () => {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loadingMore, setLoadingMore] = useState(false);
+  const [allNotes, setAllNotes] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [newNote, setNewNote] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
@@ -21,10 +24,17 @@ const NoteScreen = () => {
   }, []);
 
   const fetchNotes = async () => {
-    setLoading(true);
+    // setLoading(true);
+    if (loading || loadingMore) return;
+
     try {
+      if (page === 1) {
+        setLoading(true);
+      } else {
+        setLoadingMore(true);
+      }
       const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts"
+        `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=10`
       );
       if (!response.ok) {
         return setError("Failed to fetch notes");
@@ -36,11 +46,18 @@ const NoteScreen = () => {
           text: note.title,
         };
       });
-      setNotes(formattedNotes);
+      // setNotes(formattedNotes);
+      setAllNotes((prevNotes) =>
+        page === 1 ? formattedNotes : [...prevNotes, ...formattedNotes]
+      );
+
+      setHasMore(data.length === 10);
+      setPage((prevPage) => prevPage + 1);
     } catch (error) {
       setError(error.message);
     } finally {
       setLoading(false);
+      setLoadingMore(false);
     }
   };
 
@@ -68,7 +85,7 @@ const NoteScreen = () => {
 
       const data = await response.json();
 
-      setNotes((prevNotes) => [
+      setAllNotes((prevNotes) => [
         {
           id: data.id.toString(),
           text: data.title,
@@ -86,7 +103,7 @@ const NoteScreen = () => {
   };
   return (
     <View style={styles.container}>
-      {loading ? (
+      {loading && page === 1 ? (
         <View style={styles.loading}>
           <ActivityIndicator size="large" />
         </View>
@@ -94,7 +111,14 @@ const NoteScreen = () => {
         <Text style={styles.error}>{error}</Text>
       ) : (
         <>
-          <NoteList notes={notes} />
+          <NoteList
+            notes={allNotes}
+            onEndReached={() => {
+              if (hasMore) fetchNotes();
+            }}
+            loadingMore={loadingMore}
+          />
+
           <TouchableOpacity
             style={styles.addButton}
             onPress={() => setModalVisible(true)}
--- components/NoteList.jsx
+++ components/NoteList.jsx
@@ -1,16 +1,37 @@
-import { FlatList, View } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, View } from "react-native";
 import NoteItem from "./NoteItem";
 
-const NoteList = ({ notes }) => {
+const NoteList = ({ notes, onEndReached, loadingMore }) => {
   return (
     <View style={{ flex: 1 }}>
       <FlatList
         data={notes}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <NoteItem note={item} />}
+        onEndReached={onEndReached}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={
+          loadingMore ? (
+            <View style={styles.loadingContainer}>
+              <ActivityIndicator size="small" />
+            </View>
+          ) : null
+        }
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default NoteList;
