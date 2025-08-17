d16b5d02b67beeb4d0738ead5e7e6c3676a4e6dc
mehdimf96 <mehranfar.7575@gmail.com>
2025-08-17 10:07:04 +0330

    fetching notes from api and loading and error done

--- app/notes/index.jsx
+++ app/notes/index.jsx
@@ -1,25 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
-  View,
-  Text,
+  ActivityIndicator,
   StyleSheet,
+  Text,
   TouchableOpacity,
-  Modal,
-  TextInput,
+  View,
 } from "react-native";
-import NoteList from "../../components/NoteList";
 import AddNoteModal from "../../components/AddNoteModal";
+import NoteList from "../../components/NoteList";
 
 const NoteScreen = () => {
-  const [notes, setNotes] = useState([
-    { id: "1", text: "Note One" },
-    { id: "2", text: "Note Two" },
-    { id: "3", text: "Note Three" },
-  ]);
-
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState([]);
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      if (!response.ok) {
+        return setError("Failed to fetch notes");
+      }
+      const data = await response.json();
+      const formattedNotes = data.slice(0, 10).map((note) => {
+        return {
+          id: note.id.toString(),
+          text: note.title,
+        };
+      });
+      setNotes(formattedNotes);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const addNote = () => {
     if (newNote.trim() === "") return;
@@ -35,21 +58,30 @@ const NoteScreen = () => {
 
   return (
     <View style={styles.container}>
-      <NoteList notes={notes} />
-      <TouchableOpacity
-        style={styles.addButton}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.addButtonText}>+ Add Note</Text>
-      </TouchableOpacity>
-
-      <AddNoteModal
-        setModalVisible={setModalVisible}
-        modalVisible={modalVisible}
-        newNote={newNote}
-        setNewNote={setNewNote}
-        addNote={addNote}
-      />
+      {loading ? (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" />
+        </View>
+      ) : error ? (
+        <Text style={styles.error}>{error}</Text>
+      ) : (
+        <>
+          <NoteList notes={notes} />
+          <TouchableOpacity
+            style={styles.addButton}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.addButtonText}>+ Add Note</Text>
+          </TouchableOpacity>
+          <AddNoteModal
+            setModalVisible={setModalVisible}
+            modalVisible={modalVisible}
+            newNote={newNote}
+            setNewNote={setNewNote}
+            addNote={addNote}
+          />
+        </>
+      )}
     </View>
   );
 };
